7e4bfce461e4ecc35115e02b51a0f2ed65988a08
chandru98c <santhosh98c@gmail.com>
2024-06-15 20:56:41 +0530

    before-fonts

--- src/components/user-informaiton/Userinfo.jsx
+++ src/components/user-informaiton/Userinfo.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './user_info.css' 
-import p1 from '
+import p1 from '../../assets/images/p1.jpg'
 
 
 export default function User_info() {
@@ -8,7 +8,11 @@ export default function User_info() {
   <>
   
   <div className='user-info-container'>
-    <div>
+    <div  className='user-info-elements'>
+      <h3>Profile Info</h3>
+      <img src={p1} style={{width:'140px'}} alt="" />
+      <h2>Gwen</h2>
+      <p>Offline</p>
 
     </div>
     </div></>
